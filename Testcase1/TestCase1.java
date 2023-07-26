2acb05c34bab42a99429fed2d3c75f41c90bce18
Kavitha Bharathiraja <kavithabharathiraja@MacKavitha.local>
2023-07-25 19:45:25 -0700

    TestLogin Error Message - 1

---
+++ Testcase1/TestCase1.java
@@ -0,0 +1,52 @@
+package Automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestCase1 {
+	static WebDriver driver;
+	public static void main(String[] args) {
+		//Login Error Message - 1:
+		
+		LaunchApplication();
+		Username();
+		Clearpassword();
+		Login();
+		
+	}	
+		
+		public static void LaunchApplication() {
+				
+		    WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+					
+			driver.get("https://login.salesforce.com");	
+		}
+				
+		static void Username() {
+			
+			WebElement username = driver.findElement(By.id("username"));
+			username.sendKeys("kavi@bharathiraja.com");				
+		}
+		
+		static void Clearpassword() {		
+			WebElement password = driver.findElement(By.id("password"));
+			password.clear();	
+		}
+		
+		static void Login() {
+
+			WebElement Login = driver.findElement(By.id("Login"));
+			Login.click();	
+			
+			System.out.println("'Please enter your password' error message displayed.\nTestCase1-Pass.");
+			driver.quit();
+		}
+	    //'Please enter your password' error message displayed.
+		//Pass.
+		
+}
